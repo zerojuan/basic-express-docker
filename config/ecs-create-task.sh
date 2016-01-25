51eaae59a514710b2b8bd29f286f449a5d701e36
zerojuan <juliuscebreros@gmail.com>
2016-01-26 09:38:56 +1300

    Add ecs task script

---
+++ config/ecs-create-task.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SERVICE_NAME="express-test-service"
+IMAGE_VERSION="v_"${BUILD_NUMBER}
+TASK_FAMILY="express-test"
+
+# Create a new task definition for this build
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ./config/basic-express.json > basic-express-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family basic-express --cli-input-json file://basic-express-v_${BUILD_NUMBER}.json
+
+# Update the service with the new task definition and desired count
+TASK_REVISION=`aws ecs describe-task-definition --task-definition basic-express | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
+if [ ${DESIRED_COUNT} = "0" ]; then
+    DESIRED_COUNT="1"
+fi
+
+aws ecs update-service --cluster default --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
