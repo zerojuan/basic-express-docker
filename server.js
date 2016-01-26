f8409c5e71d719fc626ad13f3f5f2880b51d163d
zerojuan <juliuscebreros@gmail.com>
2016-01-27 12:33:45 +1300

    Another change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Is this the newest version? Maybe this one?');
+  res.send('Can you believe this?');
 });
 
 app.listen(PORT);
