1ac75e3e02acd815b033c8a286d62d64ec300380
zerojuan <juliuscebreros@gmail.com>
2016-01-28 15:48:45 +1300

    Add awscli

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 'use strict';
 
-const express = require('express');
+const express = require( 'express' );
+const aws = require( 'aws-sdk' );
 
 // Constants
 const PORT = 8080;
 
+var s3 = new AWS.S3();
+
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('This is 6th?');
+  // query an S3 object
+  s3.getListBuckets({}, function( err, data ) {
+    if ( err) {
+      return req.send( err );
+    }
+
+    return req.send( data.Buckets );
+  });
 });
 
 app.listen(PORT);
